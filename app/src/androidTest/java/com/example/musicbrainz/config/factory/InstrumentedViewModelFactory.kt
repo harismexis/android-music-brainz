cf37e600bb8b215bc189d30baa109321234b8377
Haris Mexis <theoharismexis@hotmail.com>
2021-08-06 09:05:24 +0100

    updates

--- app/src/androidTest/java/com/example/musicbrainz/config/factory/InstrumentedViewModelFactory.kt
+++ app/src/androidTest/java/com/example/musicbrainz/config/factory/InstrumentedViewModelFactory.kt
@@ -5,13 +5,11 @@ import androidx.lifecycle.ViewModelProvider
 import com.example.musicbrainz.presentation.screens.detail.viewmodel.DetailViewModel
 import com.example.musicbrainz.presentation.screens.home.viewmodel.HomeViewModel
 import dagger.Binds
-import dagger.MapKey
 import dagger.Module
 import io.mockk.mockk
 import javax.inject.Inject
 import javax.inject.Provider
 import javax.inject.Singleton
-import kotlin.reflect.KClass
 
 val mockHomeViewModel: HomeViewModel = mockk(relaxed = true)
 val mockDetailViewModel: DetailViewModel = mockk(relaxed = true)
@@ -21,15 +19,6 @@ class InstrumentedViewModelFactory @Inject constructor() : ViewModelProvider.Fac
     override fun <T : ViewModel?> create(modelClass: Class<T>) =
         getMockViewModelsMap()[modelClass]?.get() as T
 }
-
-@Target(
-    AnnotationTarget.FUNCTION,
-    AnnotationTarget.PROPERTY_GETTER,
-    AnnotationTarget.PROPERTY_SETTER
-)
-@kotlin.annotation.Retention(AnnotationRetention.RUNTIME)
-@MapKey
-internal annotation class ViewModelKey(val value: KClass<out ViewModel>)
 
 @Module
 abstract class InstrumentedViewModelModule {
--- app/src/main/java/com/example/musicbrainz/framework/di/ui/FragmentBindingsModule.kt
+++ app/src/main/java/com/example/musicbrainz/framework/di/ui/FragmentBindingsModule.kt
@@ -1,17 +1,12 @@
 package com.example.musicbrainz.framework.di.ui
 
-import com.example.musicbrainz.framework.base.BaseFragment
 import com.example.musicbrainz.presentation.screens.detail.fragment.DetailFragment
 import com.example.musicbrainz.presentation.screens.home.fragment.HomeFragment
-
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
 
 @Module
 abstract class FragmentBindingsModule {
-
-    @ContributesAndroidInjector
-    abstract fun baseFragment(): BaseFragment
 
     @ContributesAndroidInjector
     abstract fun homeFragment(): HomeFragment
