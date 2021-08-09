1cfce803e2ea0a94adbc9b5268675c399c317040
Haris Mexis <theoharismexis@hotmail.com>
2021-08-09 20:37:55 +0100

    updates

--- core/src/main/java/com/example/musicbrainz/data/repository/MusicBrainzRemoteRepository.kt
+++ core/src/main/java/com/example/musicbrainz/data/repository/MusicBrainzRemoteRepository.kt
@@ -4,7 +4,7 @@ import com.example.musicbrainz.data.datasource.MusicBrainzBaseRemoteDataSource
 import com.example.musicbrainz.domain.Album
 import com.example.musicbrainz.domain.Artist
 
-data class MusicBrainzRemoteRepository(
+class MusicBrainzRemoteRepository(
     private val dataSource: MusicBrainzBaseRemoteDataSource
 ) {
     suspend fun getArtists(query: String): List<Artist> = dataSource.getArtists(query)
